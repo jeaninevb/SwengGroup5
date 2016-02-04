63bc32ecb3a3b9a129319beedb6655381aabcc74
mcgroas <mcgroas@tcd.ie>
2016-02-04 14:44:53 +0000

    Merging outdated branch

---
+++ app/src/test/java/soft/swenggroup5/MainActivityTest.java
@@ -0,0 +1,22 @@
+package soft.swenggroup5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * TODO temporary storage until we split MainActivity
+ */
+public class MainActivityTest {
+
+    /**
+     *
+     */
+    @Test
+    public void test_splitFileSize()  {
+        assertEquals(0, MainActivity.splitFileSize(-1));
+        assertEquals(0, MainActivity.splitFileSize(0));
+        assertEquals(1, MainActivity.splitFileSize(1));
+        assertEquals(2, MainActivity.splitFileSize(2001));
+    }
+}
