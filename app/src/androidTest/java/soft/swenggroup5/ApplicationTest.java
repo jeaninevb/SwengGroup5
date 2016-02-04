2eb806f880d9482af5353dab001923dab783d036
Tomas Barry <tomas.barry@hotmail.com>
2016-02-04 18:47:50 +0000

    Fixed issue that was causing test to fail.

--- app/src/androidTest/java/soft/swenggroup5/ApplicationTest.java
+++ app/src/androidTest/java/soft/swenggroup5/ApplicationTest.java
@@ -1,16 +1,12 @@
 package soft.swenggroup5;
 
 import android.app.Application;
-import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
-
-import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  * TODO implement tests
  */
-@RunWith(AndroidJUnit4.class)
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
