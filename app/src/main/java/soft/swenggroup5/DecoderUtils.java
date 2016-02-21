e85da861976502db1ede446d3523eff8ac358efd
greens1 <greens1@tcd.ie>
2016-02-21 09:57:20 +0000

    Created class(only) for DecoderUtils

---
+++ app/src/main/java/soft/swenggroup5/DecoderUtils.java
@@ -0,0 +1,11 @@
+package soft.swenggroup5;
+
+/**
+ * Created by Sam on 20/02/2016.
+ */
+public class DecoderUtils {
+    public staticHashtable<String, String> decodeHeader(List<Bytes> data){
+
+    }
+
+}
