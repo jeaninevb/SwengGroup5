07dd29af2e08f9598e0ebffbc6fda31f98564ee7
Jeanine Burke <jvburke@wustl.edu>
2016-02-23 21:18:28 +0000

    New activity added to display the QR code (Last screen of the sender side). New files are ContactEncodeActivity.java(app/java/soft/swenggroup5/...) and activity_contact_encode.xml (res/layout/...)

---
+++ app/src/main/java/soft/swenggroup5/ContactEncodeActivity.java
@@ -0,0 +1,25 @@
+package soft.swenggroup5;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by jvictoriab on 2/23/16.
+ */
+public class ContactEncodeActivity  extends AppCompatActivity {
+
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_encode);
+
+
+        // Jeanine Burke:
+        // Lingfeng, go to res/activity_contact_encode.xml and insert "Done" and "Select New Contact" buttons
+        // each with specific IDs
+        // Here, Create the "Done" button referencing the button ID in res/activity_contact_encode.xml
+        // Create the "Select New Contact" button referencing the button ID in res/activity_contact_encode.xml
+        // Look at MainActivity.java to compare.
+
+    }
+}
