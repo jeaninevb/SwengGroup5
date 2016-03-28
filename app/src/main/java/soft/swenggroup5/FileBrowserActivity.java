055c4346b3bfa6478557df7cf667238644f1f41c
Tomas Barry <tomas.barry@hotmail.com>
2016-03-28 14:28:39 +0100

    Intermediate screen for selecting encoding added

--- app/src/main/java/soft/swenggroup5/FileBrowserActivity.java
+++ app/src/main/java/soft/swenggroup5/FileBrowserActivity.java
@@ -1,15 +1,13 @@
 package soft.swenggroup5;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileFilter;
